aca4c285de81448e493691eb29210c41cb5e6776
Darwin Gutierrez <darwincheese@gmail.com>
2019-07-04 22:56:27 +0200

    wowhead WIP

--- scrape.py
+++ scrape.py
@@ -1,6 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = 'https://tweakers.net/'
-response = requests.get(url)
-html = response.content
-print html
+url = 'https://wowhead.com/'
+cookies = dict(BCPermissionLevel='acceptAll')
+html = requests.get(url, headers={"User-Agent": "Mozilla/5.0", "Cookie": "cookieconsent=true"}, cookies=cookies)
+content = html.content
+
+soup = BeautifulSoup(content, 'html.parser')
+allNewsDivs = soup.find_all("div", class_="news-post-header-title-group")
+list = []
+for div in soup.find_all("div", class_="news-post-header-title-group").find_all("a"):
+    list.append(div)
+
+print(list)
